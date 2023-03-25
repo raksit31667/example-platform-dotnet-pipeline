901c94dd56d2f4849fc7671fdfe415035b0fd563
raksit31667 <raksit.m@ku.th>
2023-03-25 12:59:08 +0700

    :bug: allow build Docker base image script to be executable
    The error happens because we use the sh command to run our script. On Ubuntu, sh is a symbolic link pointing to /bin/bash.

--- tests/test_build_docker_base_images.py
+++ tests/test_build_docker_base_images.py
@@ -2,7 +2,7 @@ import subprocess
 
 def test_build_docker_base_images_step_given_docker_registry_name_should_build_docker_image():
     registry = 'exampleplatformacr.azurecr.io'
-    exit_code = subprocess.call(['sh', 'platform/steps/build_docker_base_images.sh', registry])
+    exit_code = subprocess.call(['./platform/steps/build_docker_base_images.sh', registry])
 
     docker_images_output = subprocess.check_output(['docker', 'images', '--format', '"{{ json .Repository }}"'])
     assert exit_code == 0
@@ -10,13 +10,13 @@ def test_build_docker_base_images_step_given_docker_registry_name_should_build_d
 
 def test_build_docker_base_images_step_given_another_docker_registry_name_should_build_docker_image():
     registry = 'anotherexampleplatformacr.azurecr.io'
-    exit_code = subprocess.call(['sh', 'platform/steps/build_docker_base_images.sh', registry])
+    exit_code = subprocess.call(['./platform/steps/build_docker_base_images.sh', registry])
 
     docker_images_output = subprocess.check_output(['docker', 'images', '--format', '"{{ json .Repository }}"'])
     assert exit_code == 0
     assert 'anotherexampleplatformacr.azurecr.io/base-application' in docker_images_output.decode('utf-8')
 
 def test_build_docker_base_images_step_given_no_docker_registry_name_should_exit_with_status_code_1():
-    exit_code = subprocess.call(['sh', 'platform/steps/build_docker_base_images.sh'])
+    exit_code = subprocess.call(['./platform/steps/build_docker_base_images.sh'])
 
     assert exit_code == 1

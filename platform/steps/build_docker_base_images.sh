741542f580ca4222178ae479e8290665d6bd742d
raksit31667 <raksit.m@ku.th>
2023-03-25 12:31:08 +0700

    :sparkles: add script to build Docker base images for consumers

---
+++ platform/steps/build_docker_base_images.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+DOCKER_REGISTRY=$1
+
+if [[ -z "$DOCKER_REGISTRY" ]]; then
+  echo "DOCKER_REGISTRY argument is missing."
+  exit 1
+fi
+
+export DOCKER_BUILDKIT=1
+
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $SCRIPT_DIRECTORY
+BASE_IMAGES_DIRECTORY="$(cd "$SCRIPT_DIRECTORY/../../docker_base_images" && pwd)"
+
+echo "Building application as base-application..."
+docker build -t "$DOCKER_REGISTRY/base-application" "$BASE_IMAGES_DIRECTORY/application"
+echo

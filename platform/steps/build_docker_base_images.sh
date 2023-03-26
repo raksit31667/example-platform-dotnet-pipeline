3547df5debac6ed75b2e5e4ac01cd2d7d75632f4
raksit31667 <raksit.m@ku.th>
2023-03-26 16:19:23 +0700

    :recycle: use underscore on Dockerfile directory for consistency

--- platform/steps/build_docker_base_images.sh
+++ platform/steps/build_docker_base_images.sh
@@ -12,8 +12,8 @@ fi
 export DOCKER_BUILDKIT=1
 
 SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DOCKERFILE_DIRECTORY="$(cd "$SCRIPT_DIRECTORY/../../docker/base-application" && pwd)"
+DOCKERFILE_DIRECTORY="$(cd "$SCRIPT_DIRECTORY/../../docker/base_application" && pwd)"
 
-echo "Building application as base-application..."
+echo "Building application based on base-application..."
 docker build -t "$DOCKER_REGISTRY/base-application" "$DOCKERFILE_DIRECTORY"
 echo

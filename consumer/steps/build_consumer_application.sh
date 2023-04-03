761e0ed7a1f728ccdd6505a891ca9443e610854e
raksit31667 <raksit.m@ku.th>
2023-04-03 19:25:13 +0700

    :rotating_light: fix consumer shell script lint issues
    Double quote to prevent globbing and word splitting

--- consumer/steps/build_consumer_application.sh
+++ consumer/steps/build_consumer_application.sh
@@ -10,5 +10,5 @@ BUILD_NUMBER=${4:-"0.0.0"}
 SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "$SCRIPT_DIRECTORY/dotnet/run_dotnet_command.sh" \
-  $DOCKER_REGISTRY $PROJECT_DIRECTORY \
-  "/run_dotnet_build.sh" $PACKAGE_NAME $BUILD_NUMBER
+  "$DOCKER_REGISTRY" "$PROJECT_DIRECTORY" \
+  "/run_dotnet_build.sh" "$PACKAGE_NAME" "$BUILD_NUMBER"

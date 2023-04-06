1a4fcdcc95b7d1b5e7b8b012da2bd096af9c1a30
raksit31667 <raksit.m@ku.th>
2023-04-06 19:20:44 +0700

    :bug: specify package name for building dotnet application

--- consumer/steps/dotnet/run_dotnet_build.sh
+++ consumer/steps/dotnet/run_dotnet_build.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 PACKAGE_NAME=$1
 BUILD_NUMBER=$2
 
-dotnet build -c Release
+dotnet build -c Release "$PACKAGE_NAME"
 dotnet publish -p:Version="$BUILD_NUMBER" -c Release -o app "./$PACKAGE_NAME"

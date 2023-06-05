dd084bb5171ebcea43f6758786134bfd5321d1a6
raksit31667 <raksit.m@ku.th>
2023-06-05 22:43:16 +0700

    :bricks: make terraformStateStorageAccountName dynamic for every environment

--- consumer/terraform/providers.tf
+++ consumer/terraform/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "example-platform-azure-kubernetes"
-    storage_account_name = "exampleplatformacastate"
+    storage_account_name = var.storage_account_name
     container_name       = "terraform-state"
     key                  = "${var.repository_name}.tfstate"
   }
--- consumer/terraform/terraform.tfvars
+++ consumer/terraform/terraform.tfvars
@@ -1,5 +1,6 @@
 repository_name      = "#{buildRepositoryName}#"
 aca_environment_id   = "#{acaEnvironmentId}#"
 aca_user_identity_id = "#{acaUserIdentityId}#"
+storage_account_name = "#{terraformStateStorageAccountName}#"
 acr_url              = "#{containerRegistryUrl}#"
 build_number         = "#{buildNumber}#"

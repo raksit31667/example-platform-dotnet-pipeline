c8de8f9bde79cdfc8c3b35d86310e96c3187e67e
raksit31667 <raksit.m@ku.th>
2023-06-18 20:35:15 +0700

    :white_check_mark: add unit test to cover building HTTP stub server proxy

--- tests/test_build_docker_base_images.py
+++ tests/test_build_docker_base_images.py
@@ -34,6 +34,14 @@ def test_build_docker_base_images_step_given_docker_registry_name_should_build_h
     docker_images_output = subprocess.check_output(['docker', 'images', '-q', 'exampleplatformacr.azurecr.io/http-stub-server'])
     assert docker_images_output.decode()
 
+def test_build_docker_base_images_step_given_docker_registry_name_should_build_http_stub_server_proxy_docker_image():
+    registry = 'exampleplatformacr.azurecr.io'
+    exit_code = subprocess.call(['./platform/steps/build_docker_base_images.sh', registry])
+
+    assert exit_code == 0
+    docker_images_output = subprocess.check_output(['docker', 'images', '-q', 'exampleplatformacr.azurecr.io/http-stub-server-proxy'])
+    assert docker_images_output.decode()
+
 def test_build_docker_base_images_step_given_no_docker_registry_name_should_exit_with_status_code_1():
     exit_code = subprocess.call(['./platform/steps/build_docker_base_images.sh'])
 

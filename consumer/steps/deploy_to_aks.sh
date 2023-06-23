3b85a99a524c6e2c784ffa9f7f0728bdde96e131
raksit31667 <raksit.m@ku.th>
2023-06-23 17:39:34 +0700

    :construction_worker: introduce deploy to AKS job
    Read platform's Helm chart and consumer's values file, then run Helm upgrade command in Docker in Azure DevOps Kubernetes config. Override image tag with build number and ingress path with app name

---
+++ consumer/steps/deploy_to_aks.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+AZURE_DEVOPS_KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
+
+set -euo pipefail
+
+DOCKER_REGISTRY=$1
+KUBERNETES_NAMESPACE=$2
+HELM_RELEASE_NAME=$3
+BUILD_NUMBER=$4
+INGRESS_PATH=$5
+HELM_VALUES_FILE_DIRECTORY=$6
+HELM_CHART_DIRECTORY=$7
+
+export DOCKER_BUILDKIT=1
+
+docker run \
+  --rm \
+  -e "KUBECONFIG=/.azure-devops-kubeconfig" \
+  -v "$AZURE_DEVOPS_KUBECONFIG":/.azure-devops-kubeconfig \
+  -v "$HELM_VALUES_FILE_DIRECTORY":/values.yaml \
+  -v "$HELM_CHART_DIRECTORY":/helm-chart-directory \
+  "$DOCKER_REGISTRY/helm" \
+  "/helm_upgrade.sh" \
+  "$KUBERNETES_NAMESPACE" \
+  "$HELM_RELEASE_NAME" \
+  "$BUILD_NUMBER" \
+  "$INGRESS_PATH"
